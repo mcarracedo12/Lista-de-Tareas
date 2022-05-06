c44436ca659f92713067002fbf668801daa8947d
Marina <marinacarracedo14@gmail.com>
2022-05-06 17:41:19 -0300

    DB agregada

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+// import { Task } from 'src/app/tasks';  A estos dos los lleve al taskService que despues importo
+// import { TASKS } from 'src/app/mock-app';
+import { TaskService } from 'src/app/service/task.service';
 import { Task } from 'src/app/tasks';
-import { TASKS } from 'src/app/mock-app';
+
 
 @Component({
   selector: 'app-tasks',
@@ -8,11 +11,15 @@ import { TASKS } from 'src/app/mock-app';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  Tasks: Task[]=TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private TaskService: TaskService) { }
 
   ngOnInit(): void {
+    //Like promisse
+    this.TaskService.getTasks().subscribe(tasks=>[
+      this.tasks = tasks
+    ]);
   }
 
 }
--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Task } from 'src/app/tasks';
+import { TASKS } from 'src/app/mock-app';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-
+  
   constructor() { }
+
+  getTasks(): Observable< Task[]>{
+    const task = of(TASKS);
+    return task;
+  }
 }
